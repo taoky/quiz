2ed31a28faae26227540d8db682bb2dc148a5130
taoky <taoky1999@gmail.com>
2022-02-08 18:01:23 +0800

    tui.rs impl

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,23 @@
-#[macro_use(defer)]
-extern crate scopeguard;
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use rand::prelude::SliceRandom;
+use std::{
+    env,
+    fs::File,
+    io::{self, Read},
+};
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    layout::{Constraint, Direction, Layout},
+    widgets::{Block, BorderType, Borders, Paragraph},
+    Frame, Terminal,
+};
 
-use nix::sys::termios;
-use rand::{self, seq::SliceRandom};
-use std::env;
-use std::fs::File;
-use std::io::prelude::*;
-
-fn refresh() {
-    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
-}
-
-fn wait_enter_refresh() {
-    for byte in std::io::stdin().bytes() {
-        let byte = byte.unwrap();
-        if byte == 10 {
-            break;
-        }
-    }
-    refresh();
-}
-
-fn main() {
+fn main() -> Result<(), io::Error> {
+    // read and parse file
     let args: Vec<_> = env::args().collect();
     let filename: String;
     match args.len() {
@@ -30,7 +26,7 @@ fn main() {
         }
         _ => {
             println!("Usage: {} [filename]", args[0]);
-            return;
+            return Err(io::Error::new(io::ErrorKind::Other, "No filename given"))?;
         }
     }
     let mut file = match File::open(&filename) {
@@ -79,19 +75,81 @@ fn main() {
         bank.push((question, answer));
     }
 
-    let orig_term = termios::tcgetattr(0).unwrap();
-    defer!(termios::tcsetattr(0, termios::SetArg::TCSADRAIN, &orig_term).unwrap(););
-    let mut term = termios::tcgetattr(0).unwrap();
-    term.local_flags.remove(termios::LocalFlags::ICANON); // get chars immediately
-    term.local_flags.remove(termios::LocalFlags::ECHO); // noecho
-    refresh();
+    // init terminal
+    enable_raw_mode()?;
+    let mut stdout = io::stdout();
+    execute!(stdout, EnterAlternateScreen, EnableMouseCapture)?;
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+
+    let res = run_app(&mut terminal, &mut bank);
+
+    // restroe terminal
+    disable_raw_mode()?;
+    execute!(
+        terminal.backend_mut(),
+        LeaveAlternateScreen,
+        DisableMouseCapture
+    )?;
+    terminal.show_cursor()?;
+
+    if let Err(err) = res {
+        eprintln!("{:?}", err);
+    }
+
+    Ok(())
+}
+
+fn run_app<B: Backend>(
+    terminal: &mut Terminal<B>,
+    bank: &mut Vec<(String, String)>,
+) -> io::Result<()> {
     loop {
         bank.shuffle(&mut rand::thread_rng());
-        for (question, answer) in &bank {
-            print!("问题:\n{}", question);
-            wait_enter_refresh();
-            print!("答案:\n{}", answer);
-            wait_enter_refresh();
+
+        for (question, answer) in bank.iter() {
+            let mut flip = false;
+            loop {
+                let answer = if flip { answer } else { "" };
+                terminal.draw(|f| ui(f, question, answer))?;
+
+                if let Event::Key(key) = event::read()? {
+                    if let KeyCode::Char('q') = key.code {
+                        return Ok(());
+                    }
+                    else if let KeyCode::Char(' ') = key.code {
+                        flip = !flip;
+                    }
+                    else if let KeyCode::Enter = key.code {
+                        break;
+                    }
+                }
+            }
         }
     }
+}
+
+fn ui<B: Backend>(f: &mut Frame<B>, question: &str, answer: &str) {
+    let size = f.size();
+
+    let block = Block::default()
+        .title("Quiz")
+        .borders(Borders::ALL)
+        .border_type(BorderType::Rounded);
+    f.render_widget(block, size);
+
+    let chunks = Layout::default()
+        .direction(Direction::Vertical)
+        .margin(1)
+        .constraints([Constraint::Percentage(49), Constraint::Percentage(49), Constraint::Percentage(2)].as_ref())
+        .split(size);
+
+    let block_question = Block::default().title("Question").borders(Borders::ALL);
+    let paragraph_question = Paragraph::new(question).block(block_question);
+    f.render_widget(paragraph_question, chunks[0]);
+    let block_answer = Block::default().title("Answer").borders(Borders::ALL);
+    let paragraph_answer = Paragraph::new(answer).block(block_answer);
+    f.render_widget(paragraph_answer, chunks[1]);
+    let paragraph_help = Paragraph::new("[SPACE] Show/hide answer, [ENTER] Next question, [q] Quit").block(Block::default());
+    f.render_widget(paragraph_help, chunks[2]);
 }
